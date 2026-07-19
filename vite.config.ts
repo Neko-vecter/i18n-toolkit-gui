f99c08c61d1a26b56a38b400c483383d9a81b1dc
Neko-vecter <vecterfang@icloud.com>
2026-07-18 21:50:54 -0400

    fix vite issue

--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,9 @@ export default defineConfig({
   },
   server: {
     port: 5173,
-    strictPort: false
+    strictPort: false,
+    watch: {
+      ignored: ["**/.electron-user-data/**"]
+    }
   }
 });
